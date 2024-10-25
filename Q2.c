4a34000662c2e714430b5bbe1dc955902435f4f8
Vikash Kumar <envikashkumar1@gmail.com>
2024-10-25 15:25:56 +0530

    add comments

--- Q2.c
+++ Q2.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-int main(){
+int main(){         //the main function where the program execution starts
 
-float i=2.0,j=3.0;
-int a=4,b=5;
-printf("%f %f %f",i,j,i+j);
-printf("float(2.0*int(4): %f\n", i*a);
-printf("float(2.0)/int(4): %i\n", i/a);
+float i=2.0,j=3.0;      // declare and initialize floating variables
+int a=4,b=5;            // declare and initialize int variables
+
+printf("%f %f %f",i,j,i+j);         // o/p: 2.000000 3.000000 5.000000
+printf("float(2.0*int(4): %f\n", i*a);      // o/p: 8.000000
+printf("float(2.0)/int(4): %i\n", i/a);     // o/p: 0
 }

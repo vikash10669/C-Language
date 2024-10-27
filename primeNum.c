9e9b99da97d88ac471a80b478e4362139c6618aa
Vikash Kumar <envikashkumar1@gmail.com>
2024-10-27 20:43:51 +0530

    update comments

--- primeNum.c
+++ primeNum.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 int main(){
-    int num, isPrime = 1;
-    printf("Enter the number\n");
-    scanf("%d", &num);
+    int num, isPrime = 1;    //declare an Integer "num" & "isPrime" initialized to 1(true)
+    printf("Enter the number\n");      
+    scanf("%d", &num);      //read an integer input from the user and store it in "num"
 
    if (num <= 1) {
-        isPrime = 0; // Numbers less than or equal to 1 are not prime
+        isPrime = 0;         // Numbers less than or equal to 1 are not prime
     } else {
+
         // Check for factors from 2 to sqrt(num)
-        for (int i = 2; i * i <= num; i++) {
+        for (int i = 2; i*i <= num; i++) {
             if (num % i == 0) {
-                isPrime = 0;         // Number is divisible by i, hence not prime
+                isPrime = 0;         // Number is divisible by i, hence num is not prime
                 break;
             }
         }

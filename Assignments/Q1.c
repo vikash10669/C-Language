76aa0a128ef9eb88b2d5b548af0f135133d3581d
Vikash Kumar <envikashkumar1@gmail.com>
2024-11-05 02:01:31 +0530

    update comments

--- Assignments/Q1.c
+++ Assignments/Q1.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
-void sumarr(int a[], int b[], int r[], int size) 
-{
+void sumarr(int a[], int b[], int res[], int size) {
     for (int i = 0; i < size; i++) {
-        r[i] = a[i] + b[i];     // add corresponding elements
+        res[i] = a[i] + b[i];     // add corresponding elements
     }
 }
 
 int main() {
-
-    int a[] = {3, 5, 9}; 
-    int b[] = {2, 6, -8}; 
+    int a[] = {6, 5, 9}; 
+    int b[] = {2, 8, -4}; 
     int size = 3;            // size of the arrays
-    int r[size];            // resultant array
+    int res[size];            // resultant array
 
     // Call the sumarr function
-    sumarr(a, b, r, size);
+    sumarr(a, b, res, size);
 
     // Print the result
     printf("Resultant array: ");
     for (int i = 0; i < size; i++) {
-        printf("%d ", r[i]);
+        printf("%d ", res[i]);
     }
     printf("\n");
 
     return 0;
 }
+// Output
+// Resultant array: 8 13 5
